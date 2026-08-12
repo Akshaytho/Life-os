5142234dafc233309cd570548de3c8e447fbf3b9
Akshaytho <114489458+Akshaytho@users.noreply.github.com>
2026-08-12 15:48:41 +0530

    feat: add Calendar route

---
+++ apps/web/app/calendar/page.tsx
@@ -0,0 +1,6 @@
+import { CalendarDashboard } from "../../components/calendar-dashboard";
+import { calendarSample } from "../../lib/calendar-sample-data";
+
+export default function CalendarPage() {
+  return <CalendarDashboard model={calendarSample} />;
+}
